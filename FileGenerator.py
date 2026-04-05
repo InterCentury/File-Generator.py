8f17581be5663131f07375bfdd6fb28a78bdf27e
InterCentury <234583143+InterCentury@users.noreply.github.com>
2026-04-06 02:07:13 +0600

    refactor: improve formatting and add success message in main function

--- FileGenerator.py
+++ FileGenerator.py
@@ -35,31 +35,37 @@ def create_files(base_path, file_list, handle_no_ext):
 
 
 def main():
- print("--------File Generator.py--------")
-
-# read "File List.txt"
-file_list = read_file_list("Files List.txt")
-if not file_list:
-  return
-
-# ask for out put path
-output_path = input("Enter the output path: ")
-if not os.path.exists(output_path):
-  print("Output path does not exist.")
-  return
-
-# ask for separate folder
-separate_folder = input("Do you want to create separate folder (y/n): ").lower().strip()
-if separate_folder == "y":
-  folder_name = input("Enter folder name: ").strip()
-  output_path = os.path.join(output_path, folder_name)
-  os.makedirs(output_path, exist_ok=True)
-
-
-# handle file without extensions (e.g. no .txt , .cpp , .py etc)
-handle_no_ext = input("Files without extension (skip/folder): ").lower().strip()
+    print("--------File Generator.py--------")
+    
+    # read "File List.txt"
+    file_list = read_file_list("Files List.txt")
+    if not file_list:
+      return
+    
+    # ask for out put path
+    output_path = input("Enter the output path: ")
+    if not os.path.exists(output_path):
+      print("Output path does not exist.")
+      return
+    
+    # ask for separate folder
+    separate_folder = input("Do you want to create separate folder (y/n): ").lower().strip()
+    if separate_folder == "y":
+      folder_name = input("Enter folder name: ").strip()
+      output_path = os.path.join(output_path, folder_name)
+      os.makedirs(output_path, exist_ok=True)
+    
+    
+    # handle file without extensions (e.g. no .txt , .cpp , .py etc)
+    handle_no_ext = input("Files without extension (skip/folder): ").lower().strip()
     if handle_no_ext not in ['skip', 'folder']:
-        handle_no_ext = 'skip'
-        
+                  handle_no_ext = 'skip'
+    
+    # if execution were successfull...              
+    print("\n🚀 Generating files...\n")
+    create_files(output_path, file_list, handle_no_ext)
+
+    print(f"\n🎉 Done! -> Path -> {output_path}")              
+           
 if __name__ == "__main__":
   main()
