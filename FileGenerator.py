421e77bbda33f3e9955aabd8d5866d3b12ff5daf
InterCentury <234583143+InterCentury@users.noreply.github.com>
2026-04-06 01:55:59 +0600

    feat: enhance main function to read file list and validate output path

--- FileGenerator.py
+++ FileGenerator.py
@@ -8,7 +8,18 @@ def create_files(base_path, file_list, handle_no_ext):
 
 
 def main():
+print("--------File Generator.py--------")
 
+# read "File List.txt"
+file_list = read_file_list("Files List.txt")
+if not file_list:
+  return
+
+# ask for out put path
+output_path = input("Enter the output path: ")
+if not os.path.exists(output_path):
+  print("Output path does not exist.")
+  return
 
 
 
